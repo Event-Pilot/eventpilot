89b45f040d21c61b07fa1b646c10ab4cdce3f52f
Wang <carlos_zhicheng@163.com>
2026-06-03 21:51:17 +0800

    feat(auth): add user repository

---
+++ backend/src/main/java/com/eventpilot/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.eventpilot.repository;
+
+import com.eventpilot.entity.UserEntity;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+  Optional<UserEntity> findByEmail(String email);
+
+  boolean existsByEmail(String email);
+}
